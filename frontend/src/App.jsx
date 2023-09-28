0dc2b1b05c0cbca6af02a1c7eeeb776d77924b62
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2023-09-28 15:28:50 +0530

    Frontend configured

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
+import { logo } from "./assets";
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello World!
+    </h1>
+  )
+}
+
+export default App;
