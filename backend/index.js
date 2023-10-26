ba1fcfc77989f8d119bd11da88b0334c79ab1bfd
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2023-10-26 19:46:22 +0530

    Update

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,31 @@ import * as dotenv from "dotenv";
 import cors from "cors";
 
 import connectDB from "./mongodb/connectDB.js";
+import postRoutes from "./routes/postRoutes.js";
+import quikpikRoutes from "./routes/quikpikRoutes.js";
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
+// const allowedOrigins = ["http://localhost:5173"];
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error("Not allowed by CORS"));
+//         }
+//     },
+//     credentials: true, // Allow cookies and headers with credentials
+// }));
+app.use(cors())
 app.use(express.json({ limit: "50mb" }));
 
-app.get("/", (req, res) => {
-    res.send("Hello From Backend");
-});
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/quikpik", quikpikRoutes);
+// app.get("/", (req, res) => {
+//     res.send("Hello From Backend");
+// });
 
 const startServer = async() => {
     try{
