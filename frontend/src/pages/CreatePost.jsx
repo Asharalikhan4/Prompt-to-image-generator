6f0a5bc9fa4da6004b64c816c0ac558941ee6776
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2023-12-08 02:29:37 +0530

    Api Update

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -20,7 +20,7 @@ const CreatePost = () => {
         if (form.prompt) {
           try {
             setGeneratingImg(true);
-            const response = await fetch('http://localhost:8080/api/v1/quikpik', {
+            const response = await fetch('https://college-project-server-asharalikhan4.vercel.app/api/v1/quikpik', {
               method: 'POST',
               headers: {
                 'Content-Type': 'application/json',
@@ -47,7 +47,7 @@ const CreatePost = () => {
         if(form.prompt && form.photo){
             setLoading(true);
             try{
-                const response = await fetch("http://localhost:8080/api/v1/post",{
+                const response = await fetch("https://college-project-server-asharalikhan4.vercel.app/api/v1/post",{
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json",
