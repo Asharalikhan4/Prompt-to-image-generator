a4d7e8dcc669a61e62cead5711a3e2e25545a64f
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2023-12-07 17:07:20 +0530

    Backend configured in backend

--- backend/index.js
+++ backend/index.js
@@ -9,30 +9,18 @@ import quikpikRoutes from "./routes/quikpikRoutes.js";
 dotenv.config();
 
 const app = express();
-// const allowedOrigins = ["http://localhost:5173"];
-// app.use(cors({
-//     origin: function (origin, callback) {
-//         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error("Not allowed by CORS"));
-//         }
-//     },
-//     credentials: true, // Allow cookies and headers with credentials
-// }));
 app.use(cors())
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/quikpik", quikpikRoutes);
-// app.get("/", (req, res) => {
-//     res.send("Hello From Backend");
-// });
 
 const startServer = async() => {
     try{
         connectDB(process.env.MONGODB_URL);
-        app.listen(8080, () => console.log(`Server has started on port http://localhost:8080`))
+        app.listen(8080, () => 
+        console.log(`Server has started on port http://localhost:8080`)
+        )
     } catch (error){
         console.log(error);
     }
