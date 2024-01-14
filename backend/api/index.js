ee76def72442c9d04d9ba8987fc2fa7aef8798fa
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-01-15 00:31:00 +0530

    Api and Url Updated

--- backend/api/index.js
+++ backend/api/index.js
@@ -19,7 +19,7 @@ const startServer = async() => {
     try{
         connectDB(process.env.MONGODB_URL);
         app.listen(8080, () => 
-        console.log(`Server has started on portt http://localhost:8080`)
+        console.log(`Server has started on port http://localhost:8080`)
         )
     } catch (error){
         console.log(error);
